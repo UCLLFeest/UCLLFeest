dcf0f1b42035b431664cfe465a527ea9480e7b18
croewens <contact@dries.io>
2016-02-19 14:53:12 +0100

    Config.yml Payum Routing.yml Payum AppKernel new PayumBundle composer.json Payum composer.lock Payum
    Dries

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new Payum\Bundle\PayumBundle\PayumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
